b4579048c4e1d04ae0aed1805cba107420b91d4a
Sumit Arora <er.sumitarora@gmail.com>
2021-07-05 21:54:46 -0400

    updating ui

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,17 @@ const contentsIndex = `<a href="/child-app">Angular App</a>
 <hr />
 <a href="/parent-app">Angular Nested Frame App</a>
 <hr />
-<a href="/frame.html">Angular Frame App</a>`;
+<a href="/frame.html">Angular Frame App</a>
+<hr />
+<a href="/frames.html">Angular Frame Apps</a>`;
 
-const contentsFrame = `<iframe src="/child-app" style="width: 100%; height: 500px;" />`;
+const contentsFrame = `<iframe src="/child-app" style="width: 100%; height: 300px; background: bisque;"></iframe>`;
+
+const contentsFrames = `
+  <iframe src="/parent-app" style="width: 100%; height: 500px; background: bisque;"></iframe>
+  <br/><hr/><br/>
+  <iframe src="/child-app" style="width: 100%; height: 300px; background: cornflowerblue;"></iframe>
+`;
 
 // writeFile function with filename, content and callback function
 fs.writeFile('dist/apps/index.html', contentsIndex, function (err) {
@@ -17,6 +25,12 @@ fs.writeFile('dist/apps/index.html', contentsIndex, function (err) {
 
 // writeFile function with filename, content and callback function
 fs.writeFile('dist/apps/frame.html', contentsFrame, function (err) {
+  if (err) throw err;
+  console.log('Frame File is created successfully.');
+});
+
+// writeFile function with filename, content and callback function
+fs.writeFile('dist/apps/frames.html', contentsFrames, function (err) {
   if (err) throw err;
   console.log('Frame File is created successfully.');
 });
